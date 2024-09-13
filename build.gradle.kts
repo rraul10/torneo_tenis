171bee91c96d06de03d3a24828296d29c2892781
JAVIER <javier.hernandez@alumno.iesluisvives.org>
2024-09-13 11:17:26 +0200

    Config gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,31 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.23"
+    // Plugin para la generación de código SQLDelight
+    id("app.cash.sqldelight") version "2.0.2"
+    // Plugin de serielización
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // Logger
+    implementation("org.lighthousegames:logging:1.3.0")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    // SQLDelight para SQLite
+    implementation("app.cash.sqldelight:sqlite-driver:2.0.2")
+    // Result ROP
+    implementation("com.michael-bull.kotlin-result:kotlin-result:2.0.0")
+    // Serialización JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    testImplementation("io.mockk:mockk:1.13.10")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
@@ -18,4 +34,23 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName.set("dev.javierhvicente.database")
+        }
+    }
 }
