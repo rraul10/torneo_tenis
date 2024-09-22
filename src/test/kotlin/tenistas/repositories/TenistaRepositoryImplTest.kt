0e49ab9367ff7fed5c7870841684ff66eab5e70c
Javier <javierhvicente@gmail.com>
2024-09-23 01:42:56 +0200

    Resolving conflicts

--- src/test/kotlin/tenistas/repositories/TenistaRepositoryImplTest.kt
+++ src/test/kotlin/tenistas/repositories/TenistaRepositoryImplTest.kt
@@ -1,10 +1,8 @@
 package tenistas.repositories
 
 import database.DatabaseConnection
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
 import tenistas.models.Tenista
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -30,6 +28,55 @@ class TenistaRepositoryImplTest {
             {assert(result[0].nombre == "Rafael Nadal")},
             { assertEquals(UUID.fromString("004c5d50-30a3-4416-a9c4-209b63d8f78c"), result[0].id) }
         )
+    }
+
+    @Test
+    fun saveTenista() {
+        val tenista=Tenista(
+            id = UUID.fromString("fb00de71-22ed-40ff-92d4-ad8aba256446"),
+            nombre = "TestNombre",
+            pais="TestPais",
+            altura= 180,
+            peso= 70,
+            puntos= 10,
+            mano= "ZURDO",
+            fecha_nacimiento = LocalDate.of(2024,1,1)
+        )
+
+        val result= repository.saveTenista(tenista)
+
+        Assertions.assertAll(
+            { Assertions.assertEquals(result.id, tenista.id) },
+            { Assertions.assertEquals(result.nombre, tenista.nombre) },
+            { Assertions.assertEquals(result.pais, tenista.pais) },
+            { Assertions.assertEquals(result.altura, tenista.altura) },
+            { Assertions.assertEquals(result.peso, tenista.peso) },
+            { Assertions.assertEquals(result.puntos, tenista.puntos) },
+            { Assertions.assertEquals(result.mano, tenista.mano) },
+            { Assertions.assertEquals(result.fecha_nacimiento, tenista.fecha_nacimiento) }
+        )
+    }
+
+    @Test
+    fun getTenistaByName() {
+        //arrange
+        //act
+        repository.saveTenista(Nadal)
+        val tenista = repository.getTenistaByName("Rafael Nadal")
+        //assert
+        assertNotNull(tenista)
+        assertEquals(Nadal, tenista)
+    }
+
+    @Test
+    fun getTenistaByNameNotFound() {
+        //arrange
+        //act
+        repository.saveTenista(Nadal)
+        val tenista = repository.getTenistaByName("Rafael Nad")
+        //assert
+        assertNull(tenista)
+        assertNotEquals(Nadal, tenista)
     }
 
 }
